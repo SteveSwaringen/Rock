74833beec6fa8260b24d0c36b9afa7c3ec5eac03
Matt Bronkema <matt.bronkema@sparkdevnetwork.org>
2021-04-07 15:12:03 -0400

    Backend code changes.

--- Rock/Model/FinancialPersonSavedAccount.cs
+++ Rock/Model/FinancialPersonSavedAccount.cs
@@ -289,6 +289,8 @@ namespace Rock.Model
                 }
             }
 
+            reference.AmountCurrencyCodeValueId = this.PreferredForeignCurrencyCodeValueId;
+
             return reference;
         }
 
